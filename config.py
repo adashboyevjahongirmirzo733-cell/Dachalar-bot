5cdd7d219e53c40e5b35f6694e7d5f02df655642
adashboyevjahongirmirzo733-cell <adashboyevjahongirmirzo733@gmail.com>
2026-06-27 06:05:29 +0500

    Add card details for payment acceptance
    Added card information for payment processing.

--- config.py
+++ config.py
@@ -8,4 +8,7 @@ BOT_TOKEN = "8917124993:AAFa1x_OgQJIt5dOEHVyi2BACOa0yKGnHVw"
 # Sizning Telegram ID raqamingiz (@userinfobot dan olgan)
 ADMIN_ID = 8097790244
 
-# To
+## Karta ma'lumotlari (To'lov qabul qilish uchun)
+CARD_NUMBER = "9860130175076574"  # O'zingizning karta raqamingizni yozing
+CARD_NAME = "Javohirbek E."       # Karta egasining ismi-familiyasi
+
